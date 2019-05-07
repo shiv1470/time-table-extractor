5f76ed003e8f1747d1722dd29b9be031b5524415
Shivam Gupta <40355903+shiv1470@users.noreply.github.com>
2019-05-07 23:11:10 +0530

    Update scratch_2.py
    added greedy algorithm to find color of graph

--- scratch_2.py
+++ scratch_2.py
@@ -2,6 +2,7 @@ subject_file = open("subjects.txt","r")
 n=int(subject_file.readline())
 sub=[]
 subcode={}
+colors=[0]*n
 for i in range(n):
     x=subject_file.readline().split(" -> ")
     sub.append(x)
@@ -10,8 +11,8 @@ graph=[None]*n
 for i in range(n):
     graph[i]=[0]*n
 student_file = open("students.txt" , "r")
-n=int(student_file.readline())
-for i in range(n):
+n2=int(student_file.readline())
+for i in range(n2):
     x=student_file.readline().split()
     x[1]=x[1].split(",")
     for j in x[1]:
@@ -20,3 +21,16 @@ for i in range(n):
                 continue
             graph[subcode[j][0]][subcode[k][0]]=1
             graph[subcode[k][0]][subcode[j][0]]=1
+colors[0]=1
+colorsused=1
+for i in range(1,n):
+    c=[]
+    for j in range(n):
+        if graph[i][j]==1 and colors[j] not in c:
+            c.append(colors[j])
+    for col in range(1,colorsused+2):
+        if col not in c:
+            colors[i]=col
+            colorsused=max(colorsused,col)
+            break
+print(colors)
